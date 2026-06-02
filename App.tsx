ae16261ee122d8bd4941dd7a4709caaed0f3bdce
Bianca <biancasilveriapaivarg@gmail.com.>
2026-06-02 17:16:59 -0300

    feat: add initial project setup with navigation and theming
    - Added images for UI components.
    - Created index.tsx as the entry point for the application.
    - Added documentation in leia-me.md for React Native components and navigation setup.
    - Updated package.json and package-lock.json to include navigation dependencies.
    - Implemented RoundedButton component for reusable button functionality.
    - Defined color palette in AppTheme.tsx for consistent theming.
    - Developed HomeScreen component with user input fields and navigation.
    - Configured TypeScript support with tsconfig.json.

---
+++ App.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { HomeScreen } from './src/Presentation/views/home/Home';
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  <NavigationContainer>
+    <Stack.Navigator screenOptions={{
+      headerShown: false
+    }}>
+      <Stack.Screen
+        name='Home'
+        component={HomeScreen}
+      />
+    </Stack.Navigator>
+  </NavigationContainer>
+}
+
+export default App;
---
+++ src/Presentation/Components/RoundedButton.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { TouchableOpacity, Text, StyleSheet } from "react-native";
+import { COLORS } from "../theme/AppTheme";
+
+interface Props{
+    text:string,
+    onPress?: () => void
+}
+
+export const RoundedButton = ( {text, onPress}: Props) =>{
+    return (
+        <TouchableOpacity onPress={onPress}
+        style={styles.btn}>
+            <Text style={styles.txtBtn}>{text}</Text>
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    btn:{
+        width:'100%',
+        height:50,
+        backgroundColor:COLORS.secundary,
+        alignItems:'center',
+        justifyContent:'center',
+        borderRadius:15,
+    },
+
+    txtBtn:{
+        color:COLORS.bgColor,
+        fontWeight:'bold',
+        fontSize:16,
+    }
+})
